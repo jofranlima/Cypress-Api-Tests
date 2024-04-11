7bc0ee37ca64310acfa88dcc3bea726ea6aa170b
jofranlima <jofran.alves@outlook.com>
2024-04-11 12:08:01 -0300

    fix: query

--- cypress/pages/SQL/sql.js
+++ cypress/pages/SQL/sql.js
@@ -1,20 +1,20 @@
 class sql {
 
 validadeteModeloAnalise(){
-      cy.task("queryDb", "SELECT * FROM `tb_example_analise` where id=1").then(
+      cy.task("queryDb", "SELECT * FROM `tb_example` where id=1").then(
         (result) => {
           expect(result[19].id).to.equal(22);
         }
       )}
 
       validadeteInsert(){
-       cy.task("queryDb", "SELECT * FROM `tb_example_analise` WHERE id=1 AND name = 'Teste 01'").then((result) => {
+       cy.task("queryDb", "SELECT * FROM `tb_example` WHERE id=1 AND name = 'Teste 01'").then((result) => {
           expect(result.length).to.be.greaterThan(0);
       });
       
     }
     liquidadeClintOperations(){
-      cy.task("queryDb", "UPDATE tb_duplicata SET valor_pago = valor_parcela, status_pagamento = 1, data_pagamento = CURDATE() WHERE nota_id IN (SELECT id FROM tb_example WHERE cliente_id = 1254848471)")
+      cy.task("queryDb", "UPDATE tb_dup SET valor_pago = valor_parcela, status_pagamento = 1, data_pagamento = CURDATE() WHERE nota_id IN (SELECT id FROM tb_example WHERE cliente_id = 1254848471)")
     }
   }
 
